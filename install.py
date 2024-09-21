2b45594f09c1170d8bbc21c636b759137e1bdc6c
stupd <123059883+vrified-stupd@users.noreply.github.com>
2024-09-21 18:43:23 +0800

    Add files via upload

---
+++ install.py
@@ -0,0 +1,37 @@
+# this script only works on linux and not for anything else.
+
+import sys
+import os
+
+def install(unstable : str):
+    if unstable == "0":
+        print("Starting install (Assuming Git is installed)...")
+        os.chdir(os.path.expanduser("~"))
+        os.system('git clone https://github.com/vrified-stupd/AHSh/ --branch a2.0.0')
+        os.chdir("AHSh/buildsh")
+        os.system('echo $PWD')
+        os.system('sh ./buildcom.sh')
+        os.system('make')
+        sys.exit(0)
+    elif unstable == "1":
+        print("Starting install (Assuming Git is installed)...")
+        os.chdir(os.path.expanduser("~"))
+        os.system('git clone https://github.com/vrified-stupd/AHSh.git')
+        os.chdir("AHSh/buildsh")
+        os.system('echo $PWD')
+        os.system('sh ./buildcom.sh')
+        os.system('make')
+        sys.exit(0)
+    else:
+        print("Wow, you got this error! You're dumb! (respectfully)'")
+
+
+while True:
+    unstableInput = input("Choose between unstable and release version of AHSh (0 is release and 1 is unstable) \n>: ")
+
+    if unstableInput == "0":
+        install("0")
+    elif unstableInput == "1":
+        install("1")
+    else:
+        print("INVALID OPTION")
