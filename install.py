89f773596454b59e90b25c45cda6ed98c2760183
stupd <123059883+vrified-stupd@users.noreply.github.com>
2024-09-23 09:44:05 +0800

    Update install.py

--- install.py
+++ install.py
@@ -8,6 +8,10 @@ def install(unstable : str):
         print("Starting install (Assuming Git is installed)...")
         os.chdir(os.path.expanduser("~"))
         os.system('git clone https://github.com/vrified-stupd/AHSh/ --branch a2.1.0')
+        chooseName = input("Choose a name \n>: ")
+        properties = open("AHSh/include/properties.h", "w")
+        properties.write("#ifndef PROPERTIES_H\n#define PROPERTIES_H\n\n#include \"libs.h\"\n// Shell Properties. Deleting a value/variable can make the shell have segfaults.\n\nchar *shVersion = \"a2.1.1\"; // Shell Version. You can change this.\nchar *name = \""+chooseName+"\"; // SHOULD CHANGE BASED ON THE USER INPUT DURING INSTALLATION\nchar *prefix = \">:\"; // Shell prefix, that sums it up lol\nchar *shName = \"AHSh\"; // Shell name, the name of the shells\n\n#endif") # I'm sorry you had to see this line of code
+        properties.close()
         os.chdir("AHSh/buildsh")
         os.system('echo $PWD')
         os.system('sh ./buildcom.sh')
@@ -17,6 +21,10 @@ def install(unstable : str):
         print("Starting install (Assuming Git is installed)...")
         os.chdir(os.path.expanduser("~"))
         os.system('git clone https://github.com/vrified-stupd/AHSh.git')
+        chooseName = input("Choose a name \n>: ")
+        properties = open("AHSh/include/properties.h", "w")
+        properties.write("#ifndef PROPERTIES_H\n#define PROPERTIES_H\n\n#include \"libs.h\"\n// Shell Properties. Deleting a value/variable can make the shell have segfaults.\n\nchar *shVersion = \"a2.1.1\"; // Shell Version. You can change this.\nchar *name = \""+chooseName+"\"; // SHOULD CHANGE BASED ON THE USER INPUT DURING INSTALLATION\nchar *prefix = \">:\"; // Shell prefix, that sums it up lol\nchar *shName = \"AHSh\"; // Shell name, the name of the shells\n\n#endif") # I'm sorry you had to see this line of code
+        properties.close()
         os.chdir("AHSh/buildsh")
         os.system('echo $PWD')
         os.system('sh ./buildcom.sh')
